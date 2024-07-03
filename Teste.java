1f9236633bf77a9d4ddf9243845ec9fb37d203e9
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-07-03 14:19:58 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Texto: Hello World!");
+        System.out.println("Números: " + (1 + 2));
+        System.out.println("Decimais: " + (0.2356 - 0.1000));
+        System.out.println("Booleanos: " + (true || false));
+    }
+}
