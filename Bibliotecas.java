9d440b0d4239bea212c431cad76cc73742543f48
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2024-07-04 16:59:45 -0300

    Add files via upload

---
+++ Bibliotecas.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Bibliotecas extends JPanel {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Color.WHITE);
+    }
+
+}
